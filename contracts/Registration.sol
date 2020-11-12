cc7badac8389897ef96337d964ee5c0666e0f159
Taylor O'Hara <tayoha@github.com>
2020-11-11 19:46:48 -0500

    added contract

---
+++ contracts/Registration.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Registration {
+    bool public valid;
+    string public name;
+    string public DOB;
+    string public addr;
+    string public state;
+    string public party;
+    uint public timestamp;
+    string public DLN;
+    uint public SSN;
+}
