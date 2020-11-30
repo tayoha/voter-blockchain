e9dd7e6c38e5c22c0fb61be051ac4f2c996995a4
Casper Hsu <caspertt2009@gmail.com>
2020-11-29 16:36:00 -0800

    refactor app.js and update contract

--- README.md
+++ README.md
@@ -16,9 +16,10 @@ Before cloning the project, let's make sure you have the correct dependencies.
     - On Windows you may want to try [nvm-windows](https://github.com/coreybutler/nvm-windows) or [nodist](https://github.com/marcelklehr/nodist).
 2. Clone this repository locally.
 3. Run `npm install`, if you encounter errors during this step, deleting `package-lock.json` before running the command may be helpful.
-4. Run `truffle version` in the console, verify that it's matching the correct versions of the project.
-5. Execute `npm run dev`, this will start the local client and listen to any changes on that port.
-6. Navigate to [`localhost:3000`](http://localhost:3000/) in your browser.
+4. Run `truffle compile` and open up ganache (can also run `ganache-cli` if you prefer non-GUI).
+5. Run `truffle migrate` and verify that the contracts are deployed successfully.
+6. Execute `npm run dev`, this will start the local client and listen to any changes on that port.
+7. Navigate to [`localhost:3000`](http://localhost:3000/) in your browser.
 
 ## Example
 
--- contracts/Registration.sol
+++ contracts/Registration.sol
@@ -8,19 +8,18 @@ contract Registration {
     bool valid;
     string name;
     string DOB;
-    //string addr;
-    //string party;
-    string transaction_type;
-    //uint timestamp;
+    string addr;
+    string party;
+    uint transaction_type;
+    uint timestamp;
     string hashed_ident;
     string ident_type;
   }
 
   mapping(uint => Voter) public voters;
 
-  function createVoter(bool _valid, string memory _name, string memory _DOB, /*string memory _addr, string memory _party, */
-  string memory _transaction_type, /*uint _timestamp, */string memory _hashed_ident, string memory _ident_type) public {
+  function createVoter(bool _valid, string memory _name, string memory _DOB, string memory _addr, string memory _party, uint _transaction_type, uint _timestamp, string memory _hashed_ident, string memory _ident_type) public {
     voterCount ++;
-    voters[voterCount] = Voter(_valid, _name, _DOB, /*_addr, _party, */_transaction_type, /*_timestamp, */_hashed_ident, _ident_type);
-  }
+    voters[voterCount] = Voter(_valid, _name, _DOB, _addr, _party, _transaction_type, _timestamp, _hashed_ident, _ident_type);
+    }
 }
