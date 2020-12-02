b12cbcff608e12bcb6d4ab8dc398acef0cafcff7
Taylor O'Hara <tayoha@github.com>
2020-12-01 23:36:01 -0700

    added database interaction and app functionality

---
+++ app.py
@@ -0,0 +1,84 @@
+import sqlite3
+from flask import Flask, jsonify, request, render_template, send_from_directory
+app = Flask(__name__)
+
+# initialize database to store voter information
+conn = sqlite3.connect("voters.db")
+
+c = conn.cursor()
+c.execute("""CREATE TABLE IF NOT EXISTS voters (
+            valid integer,
+            first_name text,
+            middle_name text,
+            last_name text,
+            dob text,
+            addr text,
+            transaction_type integer,
+            time_stamp integer,
+            hashed_ident text,
+            ident_type text
+        )""")
+
+conn.commit()
+conn.close()
+
+# when form submits, put user in database
+@app.route('/', methods=["GET", "POST"])
+def print_form():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        conn = sqlite3.connect("voters.db")
+        print(request.json)
+        valid_in = request.json["valid"]
+        first_name_in = request.json["first_name"]
+        middle_name_in = request.json["middle_name"]
+        last_name_in = request.json["last_name"]
+        dob_in = request.json["dob"]
+        addr_in = request.json["addr"]
+        transaction_type_in = request.json["transaction_type"]
+        time_stamp_in = request.json["time_stamp"]
+        hashed_ident_in = request.json["hashed_ident"]
+        ident_type_in = request.json["ident_type"]
+        # check if person already in database
+        cur = conn.execute(
+            "SELECT dob FROM voters WHERE first_name=(?)", ([first_name_in])
+        )
+        fetch = cur.fetchone()
+        if (not fetch):
+            # insert user into database
+            conn.execute(
+            "INSERT INTO voters(valid, first_name, middle_name, last_name, dob, addr, transaction_type, time_stamp, hashed_ident, ident_type)" +
+            "VALUES ((?), (?), (?), (?), (?), (?), (?), (?), (?), (?))", (valid_in, first_name_in, middle_name_in, last_name_in, dob_in, addr_in, transaction_type_in, time_stamp_in, hashed_ident_in, ident_type_in)
+            )
+        else:
+            conn.execute(
+            "UPDATE voters SET valid = (?), first_name = (?), middle_name = (?), last_name = (?), dob = (?), addr = (?), transaction_type = (?), time_stamp = (?), ident_type = (?) " +
+            "WHERE hashed_ident = (?)", (valid_in, first_name_in, middle_name_in, last_name_in, dob_in, addr_in, transaction_type_in, time_stamp_in, ident_type_in, hashed_ident_in)
+            )
+        # test print of database
+        cur = conn.execute(
+            "SELECT dob FROM voters WHERE first_name=(?)", ([first_name_in])
+        )
+        fetch = cur.fetchall()
+        print(fetch)
+        conn.close()
+        return "Success!", 200
+
+# handle GET requests for javascript files
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+# handle GET requests for json files
+@app.route('/json/<path:path>')
+def send_json(path):
+    return send_from_directory('build/contracts', path)
+
+# handle GET requests for json files
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('templates/assets/css', path)
+
+if __name__ == '__main__':
+    app.run(port=3000)
---
+++ js/app.js
@@ -0,0 +1,81 @@
+const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
+const transactionType = {
+  CREATE: 0,
+  UPDATE: 1,
+  DELETE: 2
+}
+
+var idenType
+const idenSelector = document.getElementById('iden-selector')
+const inputTypes = document.getElementById('input-types')
+
+idenSelector.addEventListener('change', (event) => {
+  const inputField = document.getElementById(event.target.value)
+  idenType = event.target.value
+  inputTypes.querySelectorAll('.usa-input').forEach(input => {
+    input.setAttribute('disabled', true)
+  })
+  inputField.removeAttribute('disabled')
+})
+
+const deploy = async () => {
+  // get form values
+  const raw = await $.getJSON('/json/Registration.json')
+  const abi = raw["abi"]
+  //const accounts = await web3.eth.getAccounts();
+  const first_name = document.getElementById('first-name').value
+  const middle_name = document.getElementById('middle-name').value
+  const last_name = document.getElementById('last-name').value
+  const dob = document.getElementById('dob').value
+  const address = document.getElementById('mailing-address-1').value + ' ' + document.getElementById('mailing-address-2').value + ' ' + document.getElementById('city').value + ', '
+  + document.getElementById('state').value + ' ' + document.getElementById('zip').value
+  const party = document.getElementById('party').value 
+  const ident = document.getElementById(idenType).value
+  // hash sensitive identification information
+  var hash = sha3_256.create();
+  hash.update(ident)
+  hash.update('30458340')
+  const hashed_ident = hash.hex()
+  // push voter to blockchain
+  // Contract address needs to be updated every call to migration
+  // TODO: allow option for deleting voter or updating information
+  await new web3.eth.Contract(abi, '0xD7DA4BE45e4DFC0Af1d3ccB288dC5EE9a33D0489')
+    .methods
+    .createVoter(
+      true,
+      first_name + ' ' + middle_name + ' ' + last_name,
+      dob,
+      address,
+      party,
+      transactionType.CREATE,
+      Date.now(),
+      hashed_ident,
+      idenType
+      )
+    .send({ 'from': accounts[0], 'gas': '1000000' });
+  // store voter information in local database
+  fetch('/', {
+      credentials: 'same-origin',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "valid": 1,
+        "first_name": first_name,
+        "middle_name" : middle_name,
+        "last_name": last_name,
+        "dob": dob,
+        "addr": address,
+        "transaction_type": transactionType.CREATE,
+        "time_stamp": Date.now(),
+        "hashed_ident": hashed_ident,
+        "ident_type": idenType
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+      })
+      .catch((error) => console.log(error));
+    window.location.reload(true);
+};
